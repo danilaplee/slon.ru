797d6aa74148950b0191b74bbc1b05dd40afb41b
danilaplee <fiend.of.sound@gmail.com>
2015-04-09 21:26:12 +0700

    url fix

--- lenovo/config/environment.js
+++ lenovo/config/environment.js
@@ -4,8 +4,8 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'lenovo',
     environment: environment,
-    baseURL: '/',
-    locationType: 'auto',
+    baseURL: '/specials/flex-test',
+    locationType: 'hash',
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
